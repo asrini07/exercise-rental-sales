b2214f8c9cd1303d8b30d18407eef303ae56f478
Aas Asrini <asrinirpl07@gmail.com>
2025-02-25 15:45:43 +0700

    fix action open sale order

--- models/product_template.py
+++ models/product_template.py
@@ -11,14 +11,31 @@ class ProductTemplate(models.Model):
         rent_count = self.env['product.template'].search_count([('is_rent', '=', True)])
         for rec in self:
             rec.count_rent = rent_count
+    #     for rent in self:
+    #         rent.count_rent = self.env['product.template'].search_count([('is_rent', '=', True)])
+    
 
-    def action_rental_products(self):
+    # def action_rental_products(self):
+    #     return {
+    #         "name": _("Rental Product"),
+    #         "type": "ir.actions.act_window",
+    #         "view_mode": "tree,form",
+    #         "res_model": "product.template",
+    #         "target": "current",
+    #         "domain": [("is_rent", "=", True)],
+    #         "context": {"default_is_rent": self.id}
+    #     }
+
+    def action_open_sale_order(self):
+        # Ambil ID produk.product dari product.template
+        product_ids = self.env['product.product'].search([('product_tmpl_id', '=', self.id)]).ids
+        # Cari sale.order yang memiliki order_line dengan produk terkait
+        sale_orders = self.env['sale.order'].search([('order_line.product_id', 'in', product_ids)])
         return {
-            "name": _("Rental Product"),
-            "type": "ir.actions.act_window",
-            "view_mode": "tree,form",
-            "res_model": "product.template",
-            "target": "current",
-            "domain": [("is_rent", "=", True)],
-            "context": {"default_is_rent": self.id}
+            'type': 'ir.actions.act_window',
+            'name': 'Sale Order',
+            'view_mode': 'tree,form',
+            'res_model': 'sale.order',
+            'domain': [('id', 'in', sale_orders.ids)],  # Filter hanya sale order yang terkait
+            'context': {'default_order_line.product_id': product_ids[0] if product_ids else False},  
         }
